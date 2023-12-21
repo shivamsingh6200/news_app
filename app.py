cf58a4c8566f69f7c6da12a2e860f14906c6191a
shivamsingh6200 <shivamsingh01012013@gmail.com>
2023-12-21 12:57:43 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import requests
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def index():
+    url="https://newsapi.org/v2/top-headlines?country=in&apiKey=44e06e3659d149998fd341c629c5e7ce"
+    r=requests.get(url).json()
+    cases={
+        'articles':r['articles']
+    }
+
+    return render_template("index.html", case = cases)
+
+if __name__ == "__main__":
+    app.run(debug=True)
